f767e10208848daf1346e64681bc05085d571930
Akhilesh Jain <akhilesh.taurus@gmail.com>
2021-04-09 15:29:40 +0530

    Unit test cases for save customer profile info
    Change-Id: I88cfa855c504aded4fb933299152146923133b5c
    Signed-off-by: Akhilesh Jain <akhilesh.taurus@gmail.com>

--- src/test/java/com/sap/nextgen/vlm/CorpOverviewApiV1Test.java
+++ src/test/java/com/sap/nextgen/vlm/CorpOverviewApiV1Test.java
@@ -97,6 +97,140 @@ public class CorpOverviewApiV1Test extends APITest {
 
     	
     }
+
+    @Test
+    public void testSaveMtnCompanyProfileTestForPeriodFY() {
+        mockResponseSequence("/response/TransactionsSalesADRMCloud.json");
+        JWTTokenFactory jwtTokenFactory = new JWTTokenFactory();
+    	String token = jwtTokenFactory.getJWTToken("I303399", "hartosh.singh.bugra@sap.com", "Hartosh Singh", "Bugra", "employee");
+        Map<String, List<String>> queryParams = new HashMap<>();
+        queryParams.put("mtnId", Lists.newArrayList("2443"));
+        queryParams.put("saveType", Lists.newArrayList("3"));
+        queryParams.put("id", Lists.newArrayList("2443"));
+        queryParams.put("newValue", Lists.newArrayList("0"));
+        queryParams.put("jwtToken", Lists.newArrayList(token));
+        final DataRequestBody dataRequestBody = new DataRequestBody();
+        dataRequestBody.setQueryParams(queryParams);
+
+        final Response response = target("v3/nucleus/data")
+                .path("apps/cicorpoverview")
+                .path("roles/Display")
+                .path("resources")
+                .path(DataEndpoint.SAVE_MTN_COMPANY_PROFILE_INFO.toString())
+                .request()
+                .post(Entity.json(dataRequestBody));
+        
+        assertThat(response, isOk());
+
+        final ResponseComponentDTO c4sComponentDTO = response.readEntity(ResponseComponentDTO.class);
+    }    
+
+    @Test
+    public void testSaveMtnCompanyProfileTestForPeriodTTM() {
+        mockResponseSequence("/response/TransactionsSalesADRMCloud.json");
+        JWTTokenFactory jwtTokenFactory = new JWTTokenFactory();
+    	String token = jwtTokenFactory.getJWTToken("I303399", "hartosh.singh.bugra@sap.com", "Hartosh Singh", "Bugra", "employee");
+        Map<String, List<String>> queryParams = new HashMap<>();
+        queryParams.put("mtnId", Lists.newArrayList("2443"));
+        queryParams.put("saveType", Lists.newArrayList("3"));
+        queryParams.put("id", Lists.newArrayList("2443"));
+        queryParams.put("newValue", Lists.newArrayList("1"));
+        queryParams.put("jwtToken", Lists.newArrayList(token));
+        final DataRequestBody dataRequestBody = new DataRequestBody();
+        dataRequestBody.setQueryParams(queryParams);
+
+        final Response response = target("v3/nucleus/data")
+                .path("apps/cicorpoverview")
+                .path("roles/Display")
+                .path("resources")
+                .path(DataEndpoint.SAVE_MTN_COMPANY_PROFILE_INFO.toString())
+                .request()
+                .post(Entity.json(dataRequestBody));
+        
+        assertThat(response, isOk());
+
+        final ResponseComponentDTO c4sComponentDTO = response.readEntity(ResponseComponentDTO.class);
+    }    
+
+    @Test
+    public void testSaveMtnCompanyProfileTestForDenominationThousands() {
+        mockResponseSequence("/response/TransactionsSalesADRMCloud.json");
+        JWTTokenFactory jwtTokenFactory = new JWTTokenFactory();
+    	String token = jwtTokenFactory.getJWTToken("I303399", "hartosh.singh.bugra@sap.com", "Hartosh Singh", "Bugra", "employee");
+        Map<String, List<String>> queryParams = new HashMap<>();
+        queryParams.put("mtnId", Lists.newArrayList("2443"));
+        queryParams.put("saveType", Lists.newArrayList("4"));
+        queryParams.put("id", Lists.newArrayList("2443"));
+        queryParams.put("newValue", Lists.newArrayList("1"));
+        queryParams.put("jwtToken", Lists.newArrayList(token));
+        final DataRequestBody dataRequestBody = new DataRequestBody();
+        dataRequestBody.setQueryParams(queryParams);
+
+        final Response response = target("v3/nucleus/data")
+                .path("apps/cicorpoverview")
+                .path("roles/Display")
+                .path("resources")
+                .path(DataEndpoint.SAVE_MTN_COMPANY_PROFILE_INFO.toString())
+                .request()
+                .post(Entity.json(dataRequestBody));
+        
+        assertThat(response, isOk());
+
+        final ResponseComponentDTO c4sComponentDTO = response.readEntity(ResponseComponentDTO.class);
+    }    
+
+    @Test
+    public void testSaveMtnCompanyProfileTestForDenominationMillions() {
+        mockResponseSequence("/response/TransactionsSalesADRMCloud.json");
+        JWTTokenFactory jwtTokenFactory = new JWTTokenFactory();
+    	String token = jwtTokenFactory.getJWTToken("I303399", "hartosh.singh.bugra@sap.com", "Hartosh Singh", "Bugra", "employee");
+        Map<String, List<String>> queryParams = new HashMap<>();
+        queryParams.put("mtnId", Lists.newArrayList("2443"));
+        queryParams.put("saveType", Lists.newArrayList("4"));
+        queryParams.put("id", Lists.newArrayList("2443"));
+        queryParams.put("newValue", Lists.newArrayList("2"));
+        queryParams.put("jwtToken", Lists.newArrayList(token));
+        final DataRequestBody dataRequestBody = new DataRequestBody();
+        dataRequestBody.setQueryParams(queryParams);
+
+        final Response response = target("v3/nucleus/data")
+                .path("apps/cicorpoverview")
+                .path("roles/Display")
+                .path("resources")
+                .path(DataEndpoint.SAVE_MTN_COMPANY_PROFILE_INFO.toString())
+                .request()
+                .post(Entity.json(dataRequestBody));
+        
+        assertThat(response, isOk());
+
+        final ResponseComponentDTO c4sComponentDTO = response.readEntity(ResponseComponentDTO.class);
+    }    
+    @Test
+    public void testSaveMtnCompanyProfileTestForDenominationBillions() {
+        mockResponseSequence("/response/TransactionsSalesADRMCloud.json");
+        JWTTokenFactory jwtTokenFactory = new JWTTokenFactory();
+    	String token = jwtTokenFactory.getJWTToken("I303399", "hartosh.singh.bugra@sap.com", "Hartosh Singh", "Bugra", "employee");
+        Map<String, List<String>> queryParams = new HashMap<>();
+        queryParams.put("mtnId", Lists.newArrayList("2443"));
+        queryParams.put("saveType", Lists.newArrayList("4"));
+        queryParams.put("id", Lists.newArrayList("2443"));
+        queryParams.put("newValue", Lists.newArrayList("3"));
+        queryParams.put("jwtToken", Lists.newArrayList(token));
+        final DataRequestBody dataRequestBody = new DataRequestBody();
+        dataRequestBody.setQueryParams(queryParams);
+
+        final Response response = target("v3/nucleus/data")
+                .path("apps/cicorpoverview")
+                .path("roles/Display")
+                .path("resources")
+                .path(DataEndpoint.SAVE_MTN_COMPANY_PROFILE_INFO.toString())
+                .request()
+                .post(Entity.json(dataRequestBody));
+        
+        assertThat(response, isOk());
+
+        final ResponseComponentDTO c4sComponentDTO = response.readEntity(ResponseComponentDTO.class);
+    }    
     
     @Test
     public void testJWTTokenFactory() throws ClientProtocolException, IOException {
